085b91a9552b4e83629b9665f5a88edd4455f321
William Zale <williamzale@gmail.com>
2023-02-22 17:16:02 -0500

    Create four arrays using different variable types

---
+++ arrays.rb
@@ -0,0 +1,16 @@
+# array with strings
+friends = ["Charles", "Mike", "Chris", "Sean"
+, "Will"]
+
+# array with integers
+ages = [35, 36, 34, 33
+, 38]
+
+# array with floats
+bank_balances = [100.2, 84.25, 87.28, 100000.2
+, 1.01]
+ 
+# array with booleans
+veracity = [false, true, false, false]
+
+
