99cae3ab3db02229ecac056ea9b817a3a4f7e866
William Zale <williamzale@gmail.com>
2023-02-22 17:31:53 -0500

    Add put statements to print arrays

--- arrays.rb
+++ arrays.rb
@@ -2,20 +2,23 @@
 friends = ["Charles", "Mike", "Chris", "Sean", "Will"]
 # will remove the first element, Charles, and return it
 friends.shift
+puts friends
 
 # array with integers
 ages = [35, 36, 34, 33, 38]
 # will remove the last element, 38, and return it
 ages.pop
+puts ages
 
 # array with floats
 bank_balances = [100.2, 84.25, 87.28, 100000.2, 1.01]
 # will add an additional element, 200, to position 0 and display all the elements in the array
 bank_balances.unshift(200)
+puts bank_balances
 
 # array with booleans
 veracity = [false, true, false, false]
 # will add the displayed arrays to the end of the existing array
 veracity.push(true, false, true)
-
+puts veracity
 
