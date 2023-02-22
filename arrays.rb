4ac928eacd43821ea41bbf82f31638100b910faf
William Zale <williamzale@gmail.com>
2023-02-22 17:29:47 -0500

    Add a different Ruby method to each array

--- arrays.rb
+++ arrays.rb
@@ -1,16 +1,21 @@
 # array with strings
-friends = ["Charles", "Mike", "Chris", "Sean"
-, "Will"]
+friends = ["Charles", "Mike", "Chris", "Sean", "Will"]
+# will remove the first element, Charles, and return it
+friends.shift
 
 # array with integers
-ages = [35, 36, 34, 33
-, 38]
+ages = [35, 36, 34, 33, 38]
+# will remove the last element, 38, and return it
+ages.pop
 
 # array with floats
-bank_balances = [100.2, 84.25, 87.28, 100000.2
-, 1.01]
- 
+bank_balances = [100.2, 84.25, 87.28, 100000.2, 1.01]
+# will add an additional element, 200, to position 0 and display all the elements in the array
+bank_balances.unshift(200)
+
 # array with booleans
 veracity = [false, true, false, false]
+# will add the displayed arrays to the end of the existing array
+veracity.push(true, false, true)
 
 
